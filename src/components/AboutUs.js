ae8e7db41765b2b847bb92e0b25d3f312f9c4036
Piyush Patel <piyushpatel1746@gmail.com>
2024-08-02 23:40:06 +0530

    Added Voxel Dog

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,10 +1,18 @@
+import NoSsr from "./no-ssr";
+import VoxelDog from "./voxel-dog";
 import { Words } from "./Words";
 
 const AboutUs = () => {
     return (
         <div className="max-w-screen max-h-[600px] w-full h-full bg-gradient-to-t from-[#E88DFF] to-[#2DBFFE] p-[3px] rounded-[15px]">
-            <div className="max-w-screen max-h-[600px] w-full h-full bg-[#0C162D] rounded-[12px] backdrop:blur-sm">
+            <div className="flex justify-between items-center max-w-screen max-h-[600px] w-full h-full bg-[#0C162D] rounded-[12px] backdrop:blur-sm">
+                <div>
+                    <Words/>
+                </div>
                 
+                <div>
+                <VoxelDog/>
+                </div>
             </div>
         </div>
     )
